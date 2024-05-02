45b9b7cd98017b93bd5b81bc8865441c070af779
Jan Eggenberger <j.eggenberger@federerag.ch>
2024-05-02 19:28:59 +0200

    init

---
+++ ToDo List/ToDo List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo List
+//
+//  Created by Jan Eggenberger on 02.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
