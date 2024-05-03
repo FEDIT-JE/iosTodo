d6d0c7ab6cf5063b1d6320292390cc33e3f76853
Jan Eggenberger <j.eggenberger@federerag.ch>
2024-05-03 05:49:13 +0200

    Added todo

--- ToDo List/ToDo List/ContentView.swift
+++ ToDo List/ToDo List/ContentView.swift
@@ -8,17 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
 
-#Preview {
-    ContentView()
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(todos, id: \.self) { todo in
+                    NavigationLink(destination: TodoDetailView(todo: todo)) {
+                        HStack(alignment: .center) {
+                            VStack(alignment: .leading) {
+                                Text(todo.title)
+                                    .font(.title3)
+                                Text(formatDate(todo.date))
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                            Spacer()
+                            StatusIndicator(status: todo.status)
+                        }
+                    }
+                }
+            }
+            .listStyle(.inset)
+            .padding()
+            .navigationTitle("Todo List")
+        }
+    }
+
+    private func formatDate(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        return formatter.string(from: date)
+    }
 }
